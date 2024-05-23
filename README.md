fa159a0701d2505ab941ebd0b4a1771fc92b80f4
ssasaaa <s.ramasaldi@gmail.com>
2024-05-24 00:52:14 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# LIVE_SCORE_BALLZ
+
+**Note: This app is just a UI and does not work properly.**
+
+<p align="center">
+  <img src="https://github.com/rozalyne/livescoreball/assets/67235972/ba0d3157-00f5-44db-8580-360263097822" alt="Image 1" width="45%" />
+  <img src="https://github.com/rozalyne/livescoreball/assets/67235972/740bc43f-65a5-4fb8-8282-0ca6fb8daf42" alt="Image 2" width="45%" />
+</p>
+
+## Description
+
+This Flutter project, **#livescoreball**, is developed as a campus assignment.
+The aim is to create a live score tracking app for sports enthusiasts.
+
+## Getting Started
+
+### Prerequisites
+
+Before running the app, ensure you have Flutter installed. For installation instructions,
+refer to the [official Flutter documentation](https://flutter.dev/docs/get-started/install).
+
+## Setup
+
+Follow these steps to set up and update the project:
+
+1. Clone the repository:
+    ```sh
+    git clone https://github.com/rozalyne/livescoreball.git
+    ```
+
+2. Navigate to the project directory:
+    ```sh
+    cd livescoreball
+    ```
+3. Check for outdated dependencies:
+    ```sh
+    flutter pub outdated
+    ```
+
+4. Update dependencies:
+    ```sh
+    flutter pub upgrade
+    ```
+
+## Features
+
+- **UI Only**: This app is just a user interface.
+- Live score updates for multiple sports.
+- Match schedules and fixtures.
+- Detailed match statistics.
+- User-friendly interface.
+
+## Technologies Used
+
+- Flutter
+- Dart
+- VSCode
+- ChatGPT OpenAI
+
+## Contributors
+
+- [Ricky Erlangga Handoko / Rozalyne](https://github.com/rozalyne/)
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ lib/main.dart
@@ -0,0 +1,373 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Football Match App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MatchListScreen(),
+    );
+  }
+}
+
+class MatchListScreen extends StatefulWidget {
+  @override
+  _MatchListScreenState createState() => _MatchListScreenState();
+}
+
+class _MatchListScreenState extends State<MatchListScreen> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 40), // For status bar padding
+            Text('Glad to see you,', style: TextStyle(fontSize: 16)),
+            Text('Syahrama Saldi!',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Find your favorite club',
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                prefixIcon: Icon(Icons.search),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text('Live Match',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            SizedBox(height: 10),
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Premier League', style: TextStyle(fontSize: 16)),
+                        Text("78'",
+                            style: TextStyle(fontSize: 16, color: Colors.red)),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset('assets/images/arsenal.png', height: 40),
+                        SizedBox(width: 20),
+                        RichText(
+                          text: TextSpan(
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                            children: [
+                              TextSpan(
+                                  text: '2',
+                                  style: TextStyle(color: Colors.green)),
+                              TextSpan(text: ' : '),
+                              TextSpan(text: '1'),
+                            ],
+                          ),
+                        ),
+                        SizedBox(width: 20),
+                        Image.asset('assets/images/chelsea.png', height: 40),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => LiveScoreScreen()),
+                        );
+                      },
+                      child: Text('Live'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Finished Match',
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                Icon(Icons.arrow_forward),
+              ],
+            ),
+            SizedBox(height: 10),
+            Expanded(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    FinishedMatchCard(
+                      league: 'Champions League,',
+                      date: 'Yesterday',
+                      team1Logo: 'assets/images/juventus.png',
+                      team2Logo: 'assets/images/psg.png',
+                      score: '15 : 0',
+                    ),
+                    SizedBox(width: 10),
+                    FinishedMatchCard(
+                      league: 'League B,',
+                      date: '15 July 2020',
+                      team1Logo: 'assets/images/bayern.png',
+                      team2Logo: 'assets/images/dortmund.png',
+                      score: '600 : 0',
+                    ),
+                    // Add more cards here if needed
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notifications',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class FinishedMatchCard extends StatelessWidget {
+  final String league;
+  final String date;
+  final String team1Logo;
+  final String team2Logo;
+  final String score;
+
+  const FinishedMatchCard({
+    Key? key,
+    required this.league,
+    required this.date,
+    required this.team1Logo,
+    required this.team2Logo,
+    required this.score,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> scores = score.split(' : ');
+    int team1Score = int.parse(scores[0]);
+    int team2Score = int.parse(scores[1]);
+
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(league, style: TextStyle(fontSize: 16)),
+                Text(date, style: TextStyle(fontSize: 16)),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(team1Logo, height: 40),
+                SizedBox(width: 20),
+                RichText(
+                  text: TextSpan(
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                    children: [
+                      TextSpan(
+                          text: scores[0],
+                          style: TextStyle(
+                              color: team1Score > team2Score
+                                  ? Colors.green
+                                  : Colors.black)),
+                      TextSpan(text: ' : '),
+                      TextSpan(
+                          text: scores[1],
+                          style: TextStyle(
+                              color: team2Score > team1Score
+                                  ? Colors.green
+                                  : Colors.black)),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 20),
+                Image.asset(team2Logo, height: 40),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LiveScoreScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text('Champions League'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Yesterday',
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Image.asset(
+                      'assets/images/arsenal.png',
+                      width: 50,
+                      height: 50,
+                    ),
+                    Text('Persib'),
+                  ],
+                ),
+                RichText(
+                  text: TextSpan(
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                    children: [
+                      TextSpan(
+                          text: '2', style: TextStyle(color: Colors.green)),
+                      TextSpan(text: ' : '),
+                      TextSpan(text: '1'),
+                    ],
+                  ),
+                ),
+                Column(
+                  children: [
+                    Image.asset(
+                      'assets/images/chelsea.png',
+                      width: 50,
+                      height: 50,
+                    ),
+                    Text('Chelsea'),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage(
+                  'assets/images/david.png',
+                ),
+              ),
+              title: Text('Top Player'),
+              subtitle: Text('David da Silva'),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Match Stats',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Expanded(
+              child: ListView(
+                children: [
+                  ListTile(
+                    title: Text('Shots'),
+                    trailing: Text('15'),
+                  ),
+                  ListTile(
+                    title: Text('Shots on target'),
+                    trailing: Text('7'),
+                  ),
+                  ListTile(
+                    title: Text('Possession'),
+                    trailing: Text('60%'),
+                  ),
+                  ListTile(
+                    title: Text('Fouls'),
+                    trailing: Text('12'),
+                  ),
+                  ListTile(
+                    title: Text('Corners'),
+                    trailing: Text('5'),
+                  ),
+                  ListTile(
+                    title: Text('Offsides'),
+                    trailing: Text('3'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
